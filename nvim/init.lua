5876a2469455241b7bb0c70f4a41b30e98f71dfd
Ofsouzap <ofsouzap@gmail.com>
2025-08-03 17:30:00 +0100

    nvim update lazy bootstrapping script from online

--- nvim/init.lua
+++ nvim/init.lua
@@ -93,14 +93,17 @@ vim.cmd("cnoreabbr Nterm NewTabTerm")
 
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not (vim.uv or vim.loop).fs_stat(lazypath) then
-    vim.fn.system({
-        "git",
-        "clone",
-        "--filter=blob:none",
-        "https://github.com/folke/lazy.nvim.git",
-        "--branch=stable", -- latest stable release
-        lazypath,
-    })
+    local lazyrepo = "https://github.com/folke/lazy.nvim.git"
+    local out = vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath })
+    if vim.v.shell_error ~= 0 then
+    vim.api.nvim_echo({
+        { "Failed to clone lazy.nvim:\n", "ErrorMsg" },
+        { out, "WarningMsg" },
+        { "\nPress any key to exit..." },
+    }, true, {})
+    vim.fn.getchar()
+    os.exit(1)
+    end
 end
 vim.opt.rtp:prepend(lazypath)
 
@@ -242,7 +245,7 @@ lspconfig.rust_analyzer.setup {
 }
 lspconfig.pyright.setup {}
 lspconfig.hls.setup {}
-lspconfig.tsserver.setup {}
+lspconfig.ts_ls.setup {}
 lspconfig.lua_ls.setup {}
 lspconfig.clangd.setup {}
 lspconfig.ocamllsp.setup {}
