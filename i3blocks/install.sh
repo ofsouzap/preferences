8a322eb80276bac3f40949a7f82e42089ce6a613
Ofsouzap <ofsouzap@gmail.com>
2025-09-30 18:06:45 +0100

    i3blocks weather indicator

--- i3blocks/install.sh
+++ i3blocks/install.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 SRC_DIR=$(dirname "$PWD/${BASH_SOURCE[0]}")
-for fn in "config"
+for fn in "config" "custom-scripts"
 do
+    rm -f "$HOME/.config/i3blocks/$fn"
     ln -s "$SRC_DIR/$fn" "$HOME/.config/i3blocks/$fn"
 done
