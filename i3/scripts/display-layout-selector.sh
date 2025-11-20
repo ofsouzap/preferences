30c8fd0d90c36872d549a4bc2f13e4497e08942d
Ofsouzap <ofsouzap@gmail.com>
2025-11-20 11:53:24 +0000

    change back from sway to i3 and update configs accordingly

--- i3/scripts/display-layout-selector.sh
+++ i3/scripts/display-layout-selector.sh
@@ -5,9 +5,6 @@ choice=$(printf "Monitor (Home)\nLaptop (Normal)\nLaptop (Zoomed 0.7x0.7)\nLapto
 case "$choice" in
     "Monitor (Home)")
         "$HOME/.config/i3/screenlayouts/monitor-home.sh"
-        i3-msg "workspace 1; move workspace to output HDMI-A-0"
-        i3-msg "workspace 2; move workspace to output eDP"
-        i3-msg "workspace 1; focus"
         ;;
     "Laptop (Normal)")
         "$HOME/.config/i3/screenlayouts/laptop-only.sh"
@@ -21,5 +18,5 @@ case "$choice" in
 esac
 
 # I configure the keyboard mapping here as I'll often run this script on first starting my PC
-setxkbmap -layout us -option caps:swapescape
+setxkbmap -layout us -option caps:swapescape -option compose:ralt
 
