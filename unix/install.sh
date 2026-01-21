26660f88335ebbb97523c35a9539e2a1baff8129
Ofsouzap <ofsouzap@gmail.com>
2026-01-21 13:00:50 +0000

    unix add bash extra config for using fzf setup

--- unix/install.sh
+++ unix/install.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 SRC_DIR=$(dirname "$PWD/${BASH_SOURCE[0]}")
-for fn in ".bash_aliases"
+for fn in ".bash_aliases" ".bash_extra_config"
 do
     ln -s "$SRC_DIR/$fn" "$HOME/$fn"
 done
+
+echo "[Don't forget to check that files are sourced in '.bashrc']"
+
